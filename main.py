35fedc04372597760f59ca2ecf67ca899413c719
kuhailamin <46251759+kuhailamin@users.noreply.github.com>
2021-06-08 17:37:14 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+balance=0
+Q1_AMOUNT=100
+Q2_AMOUNT=200
+Q3_AMOUNT=300
+Q4_AMOUNT=400
+Q5_AMOUNT=500
+Q6_AMOUNT=1000
+Q7_AMOUNT=2000
+Q8_AMOUNT=4000
+Q9_AMOUNT=8000
+Q10_AMOUNT=16000
+Q11_AMOUNT=32000
+Q12_AMOUNT=64000
+Q13_AMOUNT=125000
+Q14_AMOUNT=250000
+Q15_AMOUNT=500000
+Q16_AMOUNT=1000000
+
+
+
+print("Welcome to our show: Who wants to be a millionare?")
+name=input("May I know your name?")
+print("Nice to meet you, "+name+"!")
+whattodo=input("What will you do when you win the 1,000,000 AED?")
+print("Wow! How nice?")
+
+
