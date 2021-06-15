9baeafac852eeb5f82891b1dcc2cfedcbf34bd0c
kuhailamin <46251759+kuhailamin@users.noreply.github.com>
2021-06-15 17:42:43 +0400

    We added conditionals to ask for what they will do with 1M AED

--- main.py
+++ main.py
@@ -21,7 +21,17 @@ Q16_AMOUNT=1000000
 print("Welcome to our show: Who wants to be a millionare?")
 name=input("May I know your name?")
 print("Nice to meet you, "+name+"!")
-whattodo=input("What will you do when you win the 1,000,000 AED?")
-print("Wow! How nice?")
+whattodo=input("What will you do when you win the 1,000,000 AED? 1:Invest it, 2:Buy 1M meals, and feed the hungry, 3: Buy a fancy car, 4: Travel the world, 5: Keep it in the bank")
+if whattodo=="1":
+    print("Wow! Nice option! Make sure you diversify your investment")
+elif whattodo=="2":
+    print("Mashallah! God bless you! What a kind act")
+elif whattodo=="3":
+    print("Enjoy! But you can probably find something as fancy with less money")
+elif whattodo=="4":
+    print("You will take me with you, right? Enjoy anyways")
+else:
+    print("You might think it's safe to do that, but with inflation, you will lose the value of the money!")
+
 
 
